eaffe0c1280ef2f7e424b7d2703b42bbe438a8cc
dennis <beds.nets@gamil.com>
2016-02-28 09:23:31 +0000

    new version 0.0.2

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 'use strict';
 
-/**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
- */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
+exports.dist = function(p1, p2){
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
 };
